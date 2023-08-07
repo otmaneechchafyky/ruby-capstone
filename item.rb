51585f7c332f4574ac76cca15933c36b2cef8082
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-08-07 21:07:35 +0100

    Create Item class and All Item class properties,set up in the constructor method

---
+++ item.rb
@@ -0,0 +1,20 @@
+class Item
+  attr_accessor :id, :genre, :author, :source, :label, :publish_date, :archieved
+  attr_reader :related_items
+
+  def initialize(params)
+    @id = params[:id]
+    @genre = params[:genre]
+    @author = params[:author]
+    @source = params[:source]
+    @label = params[:label]
+    @publish_date = params[:publish_date]
+    @archived = params[:archived] || false
+    @related_items = []
+    super()
+  end
+
+  def add_related_items(item)
+    @related_items << item
+  end
+end
