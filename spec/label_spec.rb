543359f72fb3cdb7f87c4a4e3a42207344661089
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-08-08 17:04:18 +0100

    Create a spec for add_item method

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,17 +1,23 @@
 require_relative '../classes/label'
 
 describe Label do
+  before(:each) do
+    @label = Label.new('title 1', 'Green')
+  end
   describe 'Check properties' do
-    before(:each) do
-      @label = Label.new('title 1', 'Green')
-    end
-
     it 'should have correct title' do
       expect(@label.title).to eq('title 1')
     end
 
     it 'should have correct color' do
       expect(@label.color).to eq('Green')
+    end
+  end
+
+  describe 'add_item method' do
+    it 'should contain book item' do
+      @label.add_item('book item')
+      expect @label.items.to be ['book item']
     end
   end
 end
