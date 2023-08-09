a149bbe8b98d7067f220f60c64ce3e29df380d42
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-08-10 00:19:29 +0100

    add sql query to create author and game tables

--- schema.sql
+++ schema.sql
@@ -1,1 +1,13 @@
 --Create a schema.sql file with tables that will be analogical to the structure of the classes that you created:
+CREATE TABLE games (
+  id SERIAL PRIMARY KEY,
+  multiplayer BOOLEAN,
+  played_at_date DATE,
+  item_id INT,
+  FOREIGN KEY (item_id) REFERENCES items(id)
+);
+CREATE TABLE authors (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255)
+);
