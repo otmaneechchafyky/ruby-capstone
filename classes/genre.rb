55ff42df1a45c0031bb27af69e2bbc5c419000f0
Luis Zubia <soyzubia@gmail.com>
2023-08-10 11:34:01 -0700

    Refactor 'to_s' method, Add 'to_hash', 'to_json' and 'from_json' methods to be used on save/load data from json files

--- classes/genre.rb
+++ classes/genre.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class Genre
   attr_reader :id, :items
   attr_accessor :name
@@ -14,6 +16,26 @@ class Genre
   end
 
   def to_s
-    "#{@id.to_s.ljust(10)} | #{@name.ljust(25)}"
+    [
+      @id.to_s.ljust(10).to_s,
+      @name.ljust(25).to_s,
+      @items.size.to_s.ljust(10).to_s
+    ].join(' | ')
+  end
+
+  def to_hash(*_args)
+    {
+      id: @id,
+      name: @name
+    }
+  end
+
+  def to_json(*_args)
+    JSON.dump(to_hash)
+  end
+
+  def self.from_json(json)
+    parsed = JSON.parse(json)
+    parsed.to_h { |k, v| [k.to_sym, v] }
   end
 end
