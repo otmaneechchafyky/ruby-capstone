a7dedb89fbca02cb0a6e11d53f3baad15bbacb93
Luis Zubia <soyzubia@gmail.com>
2023-08-10 12:17:53 -0700

    Add all columns from Items to MusicAlbums table, add Foreign Keys

--- schema.sql
+++ schema.sql
@@ -1,12 +1,21 @@
 -- Create the 'music_albums' table
 CREATE TABLE music_albums (
-  item_id INT NOT NULL,
+  id SERIAL PRIMARY KEY,
+  genre_id INT NOT NULL,
+  author_id INT NOT NULL,
+  source_id INT NOT NULL,
+  label_id INT NOT NULL,
+  publish_date DATE NOT NULL,
+  archived BOOLEAN,
   on_spotify BOOLEAN NOT NULL,
-  FOREIGN KEY (item_id) REFERENCES items(id)
+  FOREIGN KEY (genre_id) REFERENCES genres(id),
+  FOREIGN KEY (author_id) REFERENCES authors(id),
+  FOREIGN KEY (source_id) REFERENCES sources(id),
+  FOREIGN KEY (label_id) REFERENCES labels(id)
 );
 
 -- Create the 'genres' table
 CREATE TABLE genres (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255),
+  name VARCHAR(255)
 );
