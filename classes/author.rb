0a0d3a204e8dc17cc5751650e3ff01b28a48ed5e
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-08-10 18:38:54 +0100

    include add-author method from item for association

---
+++ classes/author.rb
@@ -0,0 +1,27 @@
+require_relative '../item'
+class Author < Item
+  attr_accessor :id, :first_name, :last_name, :items
+
+  def initialize(params)
+    super(params)
+    @id = params[:id] || rand(1..10_000)
+    @first_name = params[:first_name]
+    @last_name = params[:last_name]
+    @items = []
+  end
+
+  def add_item(item)
+    return if @items.include?(item)
+
+    @items << item
+    item.add_author(self)
+  end
+
+  def to_hash
+    {
+      id: @id,
+      first_name: @first_name,
+      last_name: @last_name
+    }
+  end
+end
