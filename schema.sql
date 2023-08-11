2e00f6d45201b3842869b86ceaf4713a53f16676
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-08-11 17:58:39 +0100

    Create items table

--- schema.sql
+++ schema.sql
@@ -1,1 +1,12 @@
---Create a schema.sql file with tables that will be analogical to the structure of the classes that you created:
+--Create a schema.sql file with tables that will be analogical to the structure of the classes that we created
+
+-- Create the 'items' table
+CREATE TABLE items (
+  id SERIAL PRIMARY KEY,
+  genre VARCHAR(255),
+  author VARCHAR(255),
+  source VARCHAR(255),
+  label VARCHAR(255),
+  publish_date DATE,
+  archived BOOLEAN
+);
